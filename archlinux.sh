db30d1009df53f7b553b9c4892612617d06a954a
vctv <Serenity-Tech@outlook.com>
2025-02-28 15:19:41 +0800

    test

--- archlinux.sh
+++ archlinux.sh
@@ -2,7 +2,9 @@
 
 # minimal Arch Linux install
 # BOOT_DISK USERNAME PASSWORD desktop
-
+BOOT_DISK="/dev/nvme0n1"
+USERNAME="cool"
+PASSWORD="looc"
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 hwclock --systohc
 
@@ -17,11 +19,11 @@ echo "archlinux" > /etc/hostname
 echo -e "127.0.0.1  localhost\n::1  localhost\n127.0.1.1 ArchLinux.localdomain  ArchLinux" >> /etc/hosts
 
 # set the root password
-echo "root:$3" | chpasswd
+echo "root:$PASSWORD" | chpasswd
 
 # create a new user
-useradd -m -G wheel -s /bin/bash "$2"
-echo "$2:$3" | chpasswd
+useradd -m -G wheel -s /bin/bash "$USERNAME"
+echo "$USERNAME:$PASSWORD" | chpasswd
 echo "%wheel ALL=(ALL) ALL" >> /etc/sudoers
 
 # install the cpu ucode
@@ -39,7 +41,7 @@ if [ -d "/sys/firmware/efi/efivars" ]; then
     grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=Arch --recheck
 else
     pacman -S --noconfirm grub efivar os-prober
-    grub-install --target=i386-pc $1
+    grub-install --target=i386-pc $BOOT_DISK
 fi
 
 sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=".*quiet.*"/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=5 nowatchdog"/' /etc/default/grub
@@ -47,19 +49,19 @@ echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 # install the desktop environment
-if [ -n "$4" ]; then
-    if [ "$4"  = "KDE" ]; then
-        pacman -S --noconfirm plasma kdebase kdeutils kdegraphics sddm
-        systemctl enable sddm
-    elif [ "$4"  = "Gnome" ]; then
-        pacman -S --noconfirm gnome gnome-terminal
-        systemctl enable gdm
-    elif [ "$4"  = "Lxde" ]; then
-        pacman -S lxde lightdm lightdm-gtk-greeter
-        systemctl enable lightdm
-    elif [ "$4"  = "Xfce" ]; then
-        pacman -S --noconfirm xfce4 xfce4-goodies xfce4-terminal lightdm lightdm-gtk-greeter
-        systemctl enable lightdm
-    fi
-fi
+#if [ -n "$4" ]; then
+#    if [ "$4"  = "KDE" ]; then
+#        pacman -S --noconfirm plasma kdebase kdeutils kdegraphics sddm
+#        systemctl enable sddm
+#    elif [ "$4"  = "Gnome" ]; then
+#        pacman -S --noconfirm gnome gnome-terminal
+#        systemctl enable gdm
+#    elif [ "$4"  = "Lxde" ]; then
+#        pacman -S lxde lightdm lightdm-gtk-greeter
+#        systemctl enable lightdm
+#    elif [ "$4"  = "Xfce" ]; then
+#        pacman -S --noconfirm xfce4 xfce4-goodies xfce4-terminal lightdm lightdm-gtk-greeter
+#        systemctl enable lightdm
+#    fi
+#fi
 exit
